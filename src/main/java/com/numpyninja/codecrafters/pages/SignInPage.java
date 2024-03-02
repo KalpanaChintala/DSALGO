5acb80051ec0a6dea35de229a710d7af5c0e7700
Sherin <Eva@bdesmarest.onmicrosoft.com>
2024-03-02 10:41:38 -0800

    fixed report

--- src/main/java/com/numpyninja/codecrafters/pages/SignInPage.java
+++ src/main/java/com/numpyninja/codecrafters/pages/SignInPage.java
@@ -1,10 +1,11 @@
 package com.numpyninja.codecrafters.pages;
-
+import java.util.List;
+import java.util.Map;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import io.cucumber.datatable.DataTable;
 
 public class SignInPage {
-
 	private WebDriver driver;
 
 	// 1. By Locators: OR
@@ -26,5 +27,17 @@ public class SignInPage {
 		return new HomePage(driver);//returns the next page
 	}
 	
+	public void userNameData(DataTable datatable) {
+		List<Map<String, String>> userName=datatable.asMaps(String.class, String.class);
+		
+		for(Map<String,String> detail:userName) {
+			String uName = detail.get("username");
+			driver.findElement(UserName).sendKeys(uName);
+		}
+		
+		}	
+		}
 	
-}
+	
+	
+
---
+++ src/test/resources/Features/Signin.feature
@@ -0,0 +1,8 @@
+Feature: Validating Login page
+
+Scenario: The user success login
+    Given The user is on Login Page
+    When The user clicks Login button after entering "username" and "password" 
+      | username       | password     | 
+      | Numpyninja | Codecrafters@123 | 
+     
